eeb7873c8b60bc11a752a2a8f05f03a36318a2d6
bondarenko-inno <yahor.bandarenka@innowise.com>
2025-07-22 15:50:39 +0300

    test fixed

--- src/test/java/org/ebndrnk/authorizationservice/AuthorizationServiceApplicationTests.java
+++ src/test/java/org/ebndrnk/authorizationservice/AuthorizationServiceApplicationTests.java
@@ -1,12 +1,13 @@
 package org.ebndrnk.authorizationservice;
 
+import org.ebndrnk.authorizationservice.config.TestContainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class AuthorizationServiceApplicationTests {
+class AuthorizationServiceApplicationTests extends TestContainersConfig {
 
     @Test
     void contextLoads() {
