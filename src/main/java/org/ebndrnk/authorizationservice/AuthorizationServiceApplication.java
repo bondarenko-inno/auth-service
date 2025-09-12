2b1e777e3142fc92444d9c45712347485322181e
bondarenko-inno <yahor.bandarenka@innowise.com>
2025-09-12 10:30:31 +0300

    actuator

--- src/main/java/org/ebndrnk/authorizationservice/AuthorizationServiceApplication.java
+++ src/main/java/org/ebndrnk/authorizationservice/AuthorizationServiceApplication.java
@@ -3,10 +3,7 @@ package org.ebndrnk.authorizationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
-})
+@SpringBootApplication
 public class AuthorizationServiceApplication {
 
     public static void main(String[] args) {
